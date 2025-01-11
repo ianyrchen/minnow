0a825bcf5ece691ea9472b86d3e27c9ba5d6e297
Ian Chen <ianychen@stanford.edu>
2025-01-11 19:07:50 +0000

    cleanup

--- src/byte_stream.cc
+++ src/byte_stream.cc
@@ -17,14 +17,9 @@ void Writer::push( string data )
     }
     uint16_t push_len = std::min(capacity_ - byte_deque_.size(), data.length());
     bytes_pushed_ += push_len;
-    //std::cout << "push_len " << push_len << std::endl;
     for (uint16_t i = 0; i < push_len; i++) {
         byte_deque_.push_back(data[i]);
     }
-    
-    //for (auto i : byte_deque_) std::cout << i;
-    //std::cout << "\n";
-
 }
 
 void Writer::close()
@@ -48,11 +43,8 @@ uint64_t Writer::bytes_pushed() const
 }
 
 string_view Reader::peek() const
-{
-    //std::cout << "\npeek\n";
-    
+{    
     static std::string next_bytes = "";
-    //cout << "next_bytes " << next_bytes << "\n";
     
     // next_bytes persists through different peek calls so must ensure reset
     if (next_bytes != "")
@@ -60,40 +52,23 @@ string_view Reader::peek() const
     
     uint16_t count = 0;
     for (char i : byte_deque_) {
-        //char byte = byte_deque_.front();
         next_bytes.push_back(i);
-        //byte_deque_.pop_front();
-        //byte_deque_.push_back(byte);
         count++;
         // to pass speed
         if (count > PEEK_LEN) 
             break;
     }
     std::string_view out{ next_bytes };
-    //std::cout << "\n" << out << "\n";
-    //next_bytes = "";
     return out;
-
-    //std::string s(byte_deque_.begin(), byte_deque_.end());
-    //std::string_view out{ s };
-    //return out;
 }
 
 void Reader::pop( uint64_t len )
 {
-
-    //std::cout << "\npopping len " << len << "\n";
-    // for (auto i : byte_deque_) std::cout << i;
-    //std::cout<<"\n";
     uint16_t pop_len = std::min(byte_deque_.size(), len);
     bytes_popped_ += pop_len;
-    //std::cout << "\npop_len " << pop_len << "\n";
     for (uint16_t i = 0; i < pop_len; i++) {
         byte_deque_.pop_front();
     }
-
-    //for (auto i : byte_deque_) std::cout << i;
-    //std::cout<<"\n";
 }
 
 bool Reader::is_finished() const
