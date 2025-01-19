ae18495c1d720f0aed8255d9914fb23b13e9bd07
Ian Chen <ianychen@stanford.edu>
2025-01-19 10:42:10 +0000

    timing out byte_stream??; got past first reassembler test

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -1,17 +1,42 @@
 #include "reassembler.hh"
 #include "debug.hh"
+#include <iostream>
 
 using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring )
 {
-  debug( "unimplemented insert({}, {}, {}) called", first_index, data, is_last_substring );
+    cout << "hi\n";
+
+    Writer& writer = output_.writer();
+    cout << writer.available_capacity() << " " << data << "\n";
+    if (writer.available_capacity() >= data.length()) {
+        if (first_index == bytes_pushed_to_output_) {
+            cout << "hi\n";
+            writer.push(data);
+            bytes_pushed_to_output_ += data.length();
+            // if overlap with stream_, delete overlapping keys of stream_
+            for (uint64_t i = first_index; i < first_index + data.length(); i++) {
+                stream_.erase(i); // delete key
+            }
+        }
+        else {
+            if (first_index >= bytes_pushed_to_output_ && first_index <= bytes_pushed_to_output_ + writer.available_capacity()) {
+                for (uint64_t i = 0; i < data.length(); i++) {
+                    stream_[first_index + i] = data[i]; // insert key
+                } 
+            }
+        }
+    }
+
+    if (is_last_substring) {
+        writer.close();
+    }
 }
 
 // How many bytes are stored in the Reassembler itself?
 // This function is for testing only; don't add extra state to support it.
 uint64_t Reassembler::count_bytes_pending() const
 {
-  debug( "unimplemented count_bytes_pending() called" );
-  return {};
+  return stream_.size();
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -1,12 +1,13 @@
 #pragma once
 
 #include "byte_stream.hh"
+#include <map>
 
 class Reassembler
 {
 public:
   // Construct Reassembler to write into given ByteStream.
-  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ) {}
+  explicit Reassembler( ByteStream&& output ) : output_( std::move( output ) ), stream_() {}
 
   /*
    * Insert a new substring to be reassembled into a ByteStream.
@@ -43,4 +44,8 @@ public:
 
 private:
   ByteStream output_;
+
+  std::map<uint64_t, char> stream_;
+  bool closed_ = false;
+  uint64_t bytes_pushed_to_output_ = 0;
 };
